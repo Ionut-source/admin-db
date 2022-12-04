1b9f4cab5a99019aac8c5e444650a39e07da0ce7
Ionut-source <cumpanasoiu.ionut@gmail.com>
2022-12-04 18:32:48 +0200

    Admin-db: formUpdate.html

--- src/main/java/com/bittnettraning/admin/web/CourseController.java
+++ src/main/java/com/bittnettraning/admin/web/CourseController.java
@@ -1,11 +1,14 @@
 package com.bittnettraning.admin.web;
 
 import com.bittnettraning.admin.entities.Course;
+import com.bittnettraning.admin.entities.Trainer;
 import com.bittnettraning.admin.services.CourseService;
+import com.bittnettraning.admin.services.TrainerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,11 +21,33 @@ public class CourseController {
     @Autowired
     private CourseService courseService;
 
-    @GetMapping("/index")
+    @Autowired
+    private TrainerService trainerService;
+
+    @GetMapping(value = "/index")
     public String courses(Model model, @RequestParam(name = "keyword", defaultValue = "") String keyword) {
         List<Course> courses = courseService.findCoursesByCourseName(keyword);
         model.addAttribute("listCourses", courses);
         model.addAttribute("keyword", keyword);
         return "course-views/courses";
+    }
+
+    @GetMapping(value = "/delete")
+    public String deleteCourse(Long courseId, String keyword) {
+        courseService.removeCourse(courseId);
+        return "redirect:/courses/index?keyword=" + keyword;
+    }
+    @GetMapping(value = "/formUpdate")
+    public String updateCourse(Model model, long courseId) {
+        Course course = courseService.findCourseById(courseId);
+        List<Trainer> trainers = trainerService.getTrainers();
+        model.addAttribute("course", course);
+        model.addAttribute("listTrainers", trainers);
+        return "course-views/formUpdate";
+    }
+    @PostMapping(value = "/save")
+    public String save(Course course){
+        courseService.createOrUpdateCourse(course);
+        return "redirect:/courses/index";
     }
 }
