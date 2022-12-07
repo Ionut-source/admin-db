4f6e30cef972787d123796505ff7ff39228743cf
Ionut-source <cumpanasoiu.ionut@gmail.com>
2022-12-07 11:15:30 +0200

    Admin-db: template.html

--- src/main/java/com/bittnettraning/admin/web/CourseController.java
+++ src/main/java/com/bittnettraning/admin/web/CourseController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping(value = "/courses")
@@ -29,7 +30,7 @@ public class CourseController {
         List<Course> courses = courseService.findCoursesByCourseName(keyword);
         model.addAttribute("listCourses", courses);
         model.addAttribute("keyword", keyword);
-        return "course-views/courses";
+        return "templates/course-views/courses";
     }
 
     @GetMapping(value = "/delete")
@@ -37,24 +38,60 @@ public class CourseController {
         courseService.removeCourse(courseId);
         return "redirect:/courses/index?keyword=" + keyword;
     }
+
     @GetMapping(value = "/formUpdate")
     public String updateCourse(Model model, long courseId) {
         Course course = courseService.findCourseById(courseId);
         List<Trainer> trainers = trainerService.getTrainers();
         model.addAttribute("course", course);
         model.addAttribute("listTrainers", trainers);
-        return "course-views/formUpdate";
+        return "templates/course-views/formUpdate";
     }
+
     @GetMapping(value = "/formCreate")
-    public String formCourses(Model model){
+    public String formCourses(Model model) {
         List<Trainer> trainers = trainerService.getTrainers();
         model.addAttribute("listTrainers", trainers);
         model.addAttribute("course", new Course());
-        return "course-views/formCreate";
+        return "templates/course-views/formCreate";
     }
+
     @PostMapping(value = "/save")
-    public String save(Course course){
+    public String save(Course course) {
         courseService.createOrUpdateCourse(course);
         return "redirect:/courses/index";
+    }
+
+    @GetMapping(value = "/index/student")
+    public String coursesForCurrentStudent(Model model) {
+        Long studentId = 1L;
+        List<Course> subscribedCourses = courseService.getCoursesForStudent(studentId);
+        List<Course> otherCourses = courseService.getAllCourses().stream().filter(course ->
+                !subscribedCourses.contains(course)).collect(Collectors.toList());
+        model.addAttribute("listCourses", subscribedCourses);
+        model.addAttribute("otherCourses", otherCourses);
+        return "templates/course-views/student-courses";
+    }
+
+    @GetMapping(value = "/enrollStudent")
+    public String enrollCurrentStudentInCourse(Long courseId) {
+        Long studentId = 1L;
+        courseService.assignStudentToCourse(courseId, studentId);
+        return "redirect:/courses/index/student";
+    }
+
+    @GetMapping(value = "/index/trainer")
+    public String coursesForCurrentTrainer(Model model) {
+        Long trainerId = 1L;
+        Trainer trainer = trainerService.findTrainerById(trainerId);
+        model.addAttribute("listCourses", trainer.getCourses());
+        return "templates/course-views/trainer-courses";
+    }
+
+    @GetMapping(value = "/trainer")
+    public String coursesByTrainerId(Model model, Long trainerId) {
+        Trainer trainer = trainerService.findTrainerById(trainerId);
+        model.addAttribute("listCourses", trainer.getCourses());
+        return "templates/course-views/trainer-courses";
     }
 }
