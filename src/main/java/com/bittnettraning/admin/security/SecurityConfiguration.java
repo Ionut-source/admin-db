70293b276b44ecfa19198f917c9be5dfe0f0e096
Ionut-source <cumpanasoiu.ionut@gmail.com>
2022-12-12 10:37:30 +0200

    Admin-db: SecurityConfiguration

---
+++ src/main/java/com/bittnettraning/admin/security/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.bittnettraning.admin.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.formLogin();
+        http.authorizeRequests().antMatchers("/").permitAll();
+
+        http.authorizeRequests().anyRequest().authenticated();
+        http.exceptionHandling().accessDeniedPage("/403");
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+}
---
+++ src/main/java/com/bittnettraning/admin/web/SecurityController.java
@@ -0,0 +1,13 @@
+package com.bittnettraning.admin.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecurityController {
+
+    @GetMapping("/403")
+    public String notAuthorized(){
+        return "403";
+    }
+}
